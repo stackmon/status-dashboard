06adb6d980a4b2d1139989756e231f190bde02fb
bakhterets <bakhterets@gmail.com>
2023-05-31 19:33:06 +0200

    pep 8 fixes

--- app/websocket/__init__.py
+++ app/websocket/__init__.py
@@ -1,8 +1,7 @@
 from flask import Blueprint
 # from flask_sock import Sock
-# 
 # sock = Sock()
 
 bp = Blueprint('websocket', __name__)
 
-from app.websocket import routes
+from app.websocket import routes # noqa
--- app/websocket/routes.py
+++ app/websocket/routes.py
@@ -1,8 +1,12 @@
-from flask import request
-import simple_websocket
-from app.websocket import bp
-# from app.websocket import sock
 from app.models import Incident
+from app.websocket import bp
+
+from flask import request
+
+import simple_websocket
+
+# from app.websocket import sock
+
 from sqlalchemy import event
 
 ws = None
@@ -11,13 +15,15 @@ connected_clients = []
 
 # @sock.route('/ws')
 # def ws_handler(ws):
-    # ws = simple_websocket.Server(request.environ)
-    # connected_clients.append(ws)
-    # while True:
-    #     for client in connected_clients:
-    #         data = client.receive()
-    #         client.send(data)
-    # return ''
+#     ws = simple_websocket.Server(request.environ)
+#     connected_clients.append(ws)
+#     while True:
+#         for client in connected_clients:
+#             data = client.receive()
+#             client.send(data)
+#     return ''
+
+
 @bp.route('/ws', websocket=True)
 def ws_handler():
     global ws
@@ -30,6 +36,7 @@ def ws_handler():
         pass
     return ''
 
+
 def create_message(target):
     return {
         "id": target.id,
@@ -40,16 +47,18 @@ def create_message(target):
         "updates": target.updates
     }
 
+
 @event.listens_for(Incident, "after_update")
 def handle_incident_update(mapper, connection, target):
     message = create_message(target)
     ws.send(message)
-    # for client in connected_clients:
-        # client.send(message)
+#    for client in connected_clients:
+#      client.send(message)
+
 
 @event.listens_for(Incident, "after_insert")
 def handle_incident_insert(mapper, connection, target):
     message = create_message(target)
     ws.send(message)
-    # for client in connected_clients:
-    #     client.send(message)
+#    for client in connected_clients:
+#        client.send(message)
