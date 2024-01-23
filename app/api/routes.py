066f16b8426fe1a56ef6dfcc728dcaa43e9f96d0
Ilia Bakhterev <bakhterets@gmail.com>
2024-01-23 12:37:25 +0100

    api/incidents route added

--- app/api/routes.py
+++ app/api/routes.py
@@ -332,3 +332,18 @@ class ApiComponentStatus(MethodView):
                             "not modifying or opening new incident"
                         )
                         return incident
+
+
+@bp.route("/v1/incidents", methods=["GET"])
+class ApiIncidents(MethodView):
+    @bp.response(200, IncidentSchema(many=True))
+    def get(self):
+        """Get all incidents
+        Retrieve a list of all incidents.
+        Example:
+        .. code-block:: console
+           curl http://localhost:5000/api/v1/incidents -X GET \\
+                -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6Ikp...'
+        """
+        incidents = db.session.scalars(db.select(Incident)).all()
+        return incidents
